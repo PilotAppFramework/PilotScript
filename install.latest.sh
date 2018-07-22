a7b3c6305e27826dff7236720ce308dfad6e9f58
LXF <240302832@qq.com>
2018-07-23 00:18:59 +0800

    Update install.latest.sh

--- install.latest.sh
+++ install.latest.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-echo 开始初始化项目
+create(){
+    # echo "创建 App 函数!"
+
+    if [ -z "$1" ]; then
+        echo "未输入AppName,使用缺省值 > Pilot"
+        AppName="Pilot"
+    else
+        AppName=$1
+    fi
+
+    echo "AppName 为 $AppName !"
+
+    
+}
+
+
+if [ -z "$1" ]; then
+    echo "请输入指令"
+    exit
+else
+    # echo "包含第一个参数 $1"
+
+    $1 $2
+fi
+
